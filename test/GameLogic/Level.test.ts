c4566fbb3f888bd5ca341ceaebd371f4c631562d
Kieran Millar <kieran@millar.org>
2019-12-04 22:08:34 +0000

    Improve tests

--- test/GameLogic/Level.test.ts
+++ test/GameLogic/Level.test.ts
@@ -3,6 +3,7 @@ import {assert} from 'chai';
 import {Level, LevelConfig} from '../../src/GameLogic/Level';
 import {FloorType} from '../../src/GameLogic/Enums';
 import {Protagonist} from '../../src/GameLogic/Entities/Protagonist';
+import * as levelJson from './helpers/level.json';
 
 describe('GameLogic.Level', () => {
 	const levelConfig: LevelConfig = {
@@ -11,24 +12,28 @@ describe('GameLogic.Level', () => {
 		playerStartX: 10,
 		playerStartY: 10,
 	};
-	const level = new Level(levelConfig);
-	level.tilesFloor.set(5, 5, FloorType.Wall);
-	level.entities.addEntity(new Protagonist());
+
+	function createTestLevel(levelConfig: LevelConfig): Level {
+		const level = new Level(levelConfig);
+		level.tilesFloor.set(5, 5, FloorType.Wall);
+		level.entities.addEntity(new Protagonist());
+		return level;
+	}
 
 	it('Level has not had new properties added', () => {
-		//console.log(JSON.stringify(level));
-		const levelJson = '{"width":20,"height":20,"playerStartX":10,"playerStartY":10,"tilesFloor":{"_width":20,"_height":20,"_squares":[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]},"entities":{"_entities":[{"type":0,"x":0,"y":0,"isPlayerControlled":true,"movesQueue":{"_position":0,"_actionSequence":[]}}]}}';
+		const level = createTestLevel(levelConfig);
 
-		assert.equal(JSON.stringify(level), levelJson, 'Level has changed, make sure to update the clone method and tests!');
+		//console.log(JSON.stringify(level));
+		assert.deepEqual(JSON.parse(JSON.stringify(level)), levelJson, 'Level has changed, make sure to update the clone method and tests!');
 	});
 
 	describe('clone', () => {
 		it('Returns an exact copy of the level.', () => {
+			const level = createTestLevel(levelConfig);
 			const levelClone = level.clone();
 
 			assert.deepEqual(level, levelClone);
 			assert.notStrictEqual(level, levelClone);
-			assert.equal(JSON.stringify(level), JSON.stringify(levelClone));
 		});
 	});
 });
