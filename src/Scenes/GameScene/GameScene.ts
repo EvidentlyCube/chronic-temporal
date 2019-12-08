4bdc9d2296977b05c3aaae4222d955fee9180b58
Maurycy Zarzycki <maurycy@evidentlycube.com>
2019-12-08 20:41:39 +0100

    Moved a few classes to a proper directory

--- src/Scenes/GameScene/GameScene.ts
+++ src/Scenes/GameScene/GameScene.ts
@@ -2,7 +2,7 @@ import {Scene} from '../../../src.common/Managers/SceneManager';
 import {GameSession} from '../../GameLogic/GameSession';
 import {Game} from '../../../src.common/Core/Game';
 import {SessionRenderer} from './SessionRenderer';
-import {GameViewManager} from './States/GameViewManager';
+import {GameViewManager} from './Views/GameViewManager';
 import {SessionController} from './SessionController';
 import {PlayerInputManager} from './PlayerInputManager';
 
