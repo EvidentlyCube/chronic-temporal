abdd39575414450e0dfe948308a67af629a3da0e
Maurycy Zarzycki <maurycy.zarzycki@mauft.com>
2019-12-11 23:40:42 +0100

    Added class for serializing and deserializing a level (#32)

---
+++ test/GameLogic/helpers/generateCompleteLevel.ts
@@ -0,0 +1,45 @@
+import {Level} from '../../../src/GameLogic/Level';
+import {EntityType, FloorType, PlayerAction} from '../../../src/GameLogic/Enums';
+import {Protagonist} from '../../../src/GameLogic/Entities/Protagonist';
+import {ActionSequence} from '../../../src/GameLogic/DataStructures/ActionSequence';
+import {Entity} from '../../../src/GameLogic/Entity';
+
+export function generateCompleteLevel(): Level {
+	const level = new Level({
+		width: 17,
+		height: 31,
+		playerStartX: 13,
+		playerStartY: 7,
+	});
+
+	addEveryEntity(level);
+	addEveryFloorTile(level);
+
+	return level;
+}
+
+function addEveryEntity(level: Level): void {
+	Object.keys(EntityType).map(parseFloat).filter(key => !isNaN(key)).forEach((type: EntityType) => {
+		switch (type) {
+			case EntityType.Protagonist:
+				insertEntity(level, new Protagonist(true));
+				insertEntity(level, new Protagonist(false, new ActionSequence([PlayerAction.MoveDown, PlayerAction.MoveUpLeft], 1)));
+				break;
+
+			default:
+				throw new Error(`generateCompleteLevel() does not handle creation of entity ${EntityType[type]}.`);
+		}
+	});
+}
+
+function addEveryFloorTile(level: Level): void {
+	Object.keys(FloorType).map(parseFloat).filter(key => !isNaN(key)).forEach((type: FloorType) => {
+		level.tilesFloor.set(type % level.width, type / level.height | 0, type);
+	});
+}
+
+function insertEntity(level: Level, entity: Entity): void {
+	entity.x = level.entities.length % level.width;
+	entity.y = Math.floor(level.entities.length / level.width);
+	level.entities.addEntity(entity);
+}
