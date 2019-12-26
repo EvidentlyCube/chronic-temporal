b3ea1f1ed44024abfe02b77a1b777b1e7c830b50
Maurycy Zarzycki <maurycy.zarzycki@mauft.com>
2019-12-26 21:05:35 +0100

    Refactored e2e tests to have all the assertions chainable (#49)

--- test/GameLogic/e2e/Fireball.test.ts
+++ test/GameLogic/e2e/Fireball.test.ts
@@ -1,6 +1,4 @@
 import 'mocha';
-import {assert} from 'chai';
-import {SessionPlayer} from '../helpers/SessionPlayer';
 import {TestLevelBuilder} from '../helpers/TestLevelBuilder';
 import {EntityType, FloorType, PlayerAction} from '../../../src/GameLogic/Enums';
 import {Pushable} from '../../../src/GameLogic/Entities/Pushable';
@@ -9,163 +7,117 @@ import {Fireball} from '../../../src/GameLogic/Entities/Fireball';
 import {Iceblock} from '../../../src/GameLogic/Entities/Iceblock';
 import {Protagonist} from '../../../src/GameLogic/Entities/Protagonist';
 import {ActionSequence} from '../../../src/GameLogic/DataStructures/ActionSequence';
-import {levelAssert} from '../helpers/levelAssert';
 
 const {getX, getY, cw90, ccw90, opposite} = Direction8Utils;
 
 describe('GameLogic.e2e - Fireball', () => {
 	Direction8Utils.allDirectional.forEach((direction) => {
 		it(`Fireball should move ${Direction8Utils.getName(direction)}`, () => {
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder.newLevel().addEntity(new Fireball(direction), 5, 5),
-				[PlayerAction.Wait],
-			);
-
-			levelAssert.assertEntityAt(level, EntityType.Fireball, 5 + getX(direction), 5 + getY(direction));
+			TestLevelBuilder.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, 5 + getX(direction), 5 + getY(direction));
 		});
 
 		it(`Fireball should turn clockwise when it encounters a wall while moving ${Direction8Utils.getName(direction)}`, () => {
 			const expectedX = 5 + getX(cw90(direction));
 			const expectedY = 5 + getY(cw90(direction));
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall),
-				[PlayerAction.Wait],
-			);
 
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder
+				.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 
 		it(`Fireball should turn clockwise when it encounters a pushable while moving ${Direction8Utils.getName(direction)}`, () => {
 			const expectedX = 5 + getX(cw90(direction));
 			const expectedY = 5 + getY(cw90(direction));
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction)),
-				[PlayerAction.Wait],
-			);
 
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder
+				.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 
 		it(`Fireball should turn ccw when it encounters a wall when trying to turn clockwise while moving ${Direction8Utils.getName(direction)}`, () => {
 			const expectedX = 5 + getX(ccw90(direction));
 			const expectedY = 5 + getY(ccw90(direction));
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
-					.plotFloor(
-						5 + getX(cw90(direction)),
-						5 + getY(cw90(direction)),
-						FloorType.Wall,
-					),
-				[PlayerAction.Wait],
-			);
-
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder
+				.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
+				.plotFloor(
+					5 + getX(cw90(direction)),
+					5 + getY(cw90(direction)),
+					FloorType.Wall,
+				)
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 
 		it(`Fireball should turn ccw when it encounters a pushable when trying to turn clockwise while moving ${Direction8Utils.getName(direction)}`, () => {
 			const expectedX = 5 + getX(ccw90(direction));
 			const expectedY = 5 + getY(ccw90(direction));
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
-					.addEntity(new Pushable(), 5 + getX(cw90(direction)), 5 + getY(cw90(direction))),
-				[PlayerAction.Wait],
-			);
 
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder
+				.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
+				.addEntity(new Pushable(), 5 + getX(cw90(direction)), 5 + getY(cw90(direction)))
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 
 		it(`Fireball should turn opposite when it encounters a wall when trying to turn ccw while moving ${Direction8Utils.getName(direction)}`, () => {
 			const expectedX = 5 + getX(opposite(direction));
 			const expectedY = 5 + getY(opposite(direction));
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
-					.plotFloor(
-						5 + getX(cw90(direction)),
-						5 + getY(cw90(direction)),
-						FloorType.Wall,
-					)
-					.plotFloor(
-						5 + getX(ccw90(direction)),
-						5 + getY(ccw90(direction)),
-						FloorType.Wall,
-					),
-				[PlayerAction.Wait],
-			);
 
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
+				.plotFloor(5 + getX(cw90(direction)), 5 + getY(cw90(direction)), FloorType.Wall)
+				.plotFloor(5 + getX(ccw90(direction)), 5 + getY(ccw90(direction)), FloorType.Wall)
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 
 		it(`Fireball should turn opposite when it encounters a pushable when trying to turn ccw while moving ${Direction8Utils.getName(direction)}`, () => {
 			const expectedX = 5 + getX(opposite(direction));
 			const expectedY = 5 + getY(opposite(direction));
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
-					.addEntity(new Pushable(), 5 + getX(cw90(direction)), 5 + getY(cw90(direction)))
-					.addEntity(new Pushable(), 5 + getX(ccw90(direction)), 5 + getY(ccw90(direction))),
-				[PlayerAction.Wait],
-			);
 
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
+				.addEntity(new Pushable(), 5 + getX(cw90(direction)), 5 + getY(cw90(direction)))
+				.addEntity(new Pushable(), 5 + getX(ccw90(direction)), 5 + getY(ccw90(direction)))
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 
 		it(`Fireball should not move when it encounters a wall when trying to turn opposite while moving ${Direction8Utils.getName(direction)}`, () => {
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
-					.plotFloor(
-						5 + getX(cw90(direction)),
-						5 + getY(cw90(direction)),
-						FloorType.Wall,
-					)
-					.plotFloor(
-						5 + getX(ccw90(direction)),
-						5 + getY(ccw90(direction)),
-						FloorType.Wall,
-					)
-					.plotFloor(
-						5 + getX(opposite(direction)),
-						5 + getY(opposite(direction)),
-						FloorType.Wall,
-					),
-				[PlayerAction.Wait],
-			);
-
-			levelAssert.assertEntityAt(level, EntityType.Fireball, 5, 5);
+			TestLevelBuilder.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.plotFloor(5 + getX(direction), 5 + getY(direction), FloorType.Wall)
+				.plotFloor(5 + getX(cw90(direction)), 5 + getY(cw90(direction)), FloorType.Wall)
+				.plotFloor(5 + getX(ccw90(direction)), 5 + getY(ccw90(direction)), FloorType.Wall)
+				.plotFloor(5 + getX(opposite(direction)), 5 + getY(opposite(direction)), FloorType.Wall)
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, 5, 5);
 		});
 
 		it(`Fireball should not move when it encounters a pushable when trying to turn opposite while moving ${Direction8Utils.getName(direction)}`, () => {
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder
-					.newLevel()
-					.addEntity(new Fireball(direction), 5, 5)
-					.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
-					.addEntity(new Pushable(), 5 + getX(cw90(direction)), 5 + getY(cw90(direction)))
-					.addEntity(new Pushable(), 5 + getX(ccw90(direction)), 5 + getY(ccw90(direction)))
-					.addEntity(new Pushable(), 5 + getX(opposite(direction)), 5 + getY(opposite(direction))),
-				[PlayerAction.Wait],
-			);
-
-			levelAssert.assertEntityAt(level, EntityType.Fireball, 5, 5);
+			TestLevelBuilder.newLevel()
+				.addEntity(new Fireball(direction), 5, 5)
+				.addEntity(new Pushable(), 5 + getX(direction), 5 + getY(direction))
+				.addEntity(new Pushable(), 5 + getX(cw90(direction)), 5 + getY(cw90(direction)))
+				.addEntity(new Pushable(), 5 + getX(ccw90(direction)), 5 + getY(ccw90(direction)))
+				.addEntity(new Pushable(), 5 + getX(opposite(direction)), 5 + getY(opposite(direction)))
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, 5, 5);
 		});
 	});
 
@@ -182,63 +134,47 @@ describe('GameLogic.e2e - Fireball', () => {
 
 	roomEdgeMoves.forEach(([x, y, direction, expectedX, expectedY]) => {
 		it(`Fireball should turn correctly when trying to move ${Direction8Utils.getName(direction)} out of bounds`, () => {
-			const [, level] = SessionPlayer.play(
-				TestLevelBuilder.newLevel().addEntity(new Fireball(direction), x, y),
-				[PlayerAction.Wait],
-			);
-
-			levelAssert.assertEntityAt(level, EntityType.Fireball, expectedX, expectedY);
+			TestLevelBuilder.newLevel()
+				.addEntity(new Fireball(direction), x, y)
+				.run(PlayerAction.Wait)
+				.assertEntityAt(EntityType.Fireball, expectedX, expectedY);
 		});
 	});
 
 	it('Fireball should destroy Protagonist when Fireball moves into the player', () => {
-		const [, level] = SessionPlayer.play(
-			TestLevelBuilder
-				.newLevel()
-				.addEntity(new Fireball(Direction8.Down), 10, 9),
-			PlayerAction.Wait,
-		);
-
-		assert.isUndefined(level.entities.getPlayer());
+		TestLevelBuilder.newLevel()
+			.addEntity(new Fireball(Direction8.Down), 10, 9)
+			.run(PlayerAction.Wait)
+			.assertEntityCount(EntityType.Protagonist, 0);
 	});
 
 	it('Fireball should destroy Protagonist when Protagonist moves into the fireball', () => {
-		const [, level] = SessionPlayer.play(
-			TestLevelBuilder
-				.newLevel()
-				.addEntity(new Fireball(Direction8.Left), 10, 9),
-			PlayerAction.MoveUp,
-		);
-
-		assert.isUndefined(level.entities.getPlayer());
+		TestLevelBuilder.newLevel()
+			.addEntity(new Fireball(Direction8.Left), 10, 9)
+			.run(PlayerAction.MoveUp)
+			.assertEntityCount(EntityType.Protagonist, 0);
 	});
 
 	it('Single fireball should exitinguish when it moves into an iceblock and melt it', () => {
-		const [, level] = SessionPlayer.play(
-			TestLevelBuilder
-				.newLevel()
-				.addEntity(new Fireball(Direction8.Left), 6, 5)
-				.addEntity(new Iceblock(new Pushable()), 5, 5),
-			PlayerAction.Wait,
-		);
-
-		levelAssert.assertEntityCount(level, EntityType.Fireball, 0);
-		levelAssert.assertEntityCount(level, EntityType.Iceblock, 0);
-		levelAssert.assertEntityAt(level, EntityType.Pushable, 5, 5);
+		TestLevelBuilder
+			.newLevel()
+			.addEntity(new Fireball(Direction8.Left), 6, 5)
+			.addEntity(new Iceblock(new Pushable()), 5, 5)
+			.run(PlayerAction.Wait)
+			.assertEntityCount(EntityType.Fireball, 0)
+			.assertEntityCount(EntityType.Iceblock, 0)
+			.assertEntityAt(EntityType.Pushable, 5, 5);
 	});
 
 	it('Multiple fireballs should exitinguish when they move into an iceblock and melt it, but contents are ejected safely', () => {
-		const [, level] = SessionPlayer.play(
-			TestLevelBuilder
-				.newLevel()
-				.addEntity(new Fireball(Direction8.Left), 6, 5)
-				.addEntity(new Fireball(Direction8.Right), 4, 5)
-				.addEntity(new Iceblock(new Protagonist(false, new ActionSequence([]))), 5, 5),
-			PlayerAction.Wait,
-		);
-
-		levelAssert.assertEntityCount(level, EntityType.Fireball, 0);
-		levelAssert.assertEntityCount(level, EntityType.Iceblock, 0);
-		levelAssert.assertEntityAt(level, EntityType.Protagonist, 5, 5);
+		TestLevelBuilder
+			.newLevel()
+			.addEntity(new Fireball(Direction8.Left), 6, 5)
+			.addEntity(new Fireball(Direction8.Right), 4, 5)
+			.addEntity(new Iceblock(new Protagonist(false, new ActionSequence([]))), 5, 5)
+			.run(PlayerAction.Wait)
+			.assertEntityCount(EntityType.Fireball, 0)
+			.assertEntityCount(EntityType.Iceblock, 0)
+			.assertEntityAt(EntityType.Protagonist, 5, 5);
 	});
 });
---
+++ test/GameLogic/helpers/SessionAsserter.ts
@@ -0,0 +1,59 @@
+import {GameSession} from '../../../src/GameLogic/GameSession';
+import {EntityType} from '../../../src/GameLogic/Enums';
+import {assert} from 'chai';
+import {Level} from '../../../src/GameLogic/Level';
+import {TurnState} from '../../../src/GameLogic/TurnState';
+
+export class SessionAsserter {
+	private readonly _session: GameSession;
+
+	private readonly _turnStates: TurnState[];
+
+	public get session(): GameSession {
+		return this._session;
+	}
+
+	public get level(): Level {
+		return this._session.level;
+	}
+
+	constructor(session: GameSession, turnStates: TurnState[]) {
+		this._session = session;
+		this._turnStates = turnStates;
+	}
+
+	public assertEntityCount(entityType: EntityType, expectedCount: number): SessionAsserter {
+		assert.lengthOf(this._session.level.entities.getEntitiesOfType(entityType), expectedCount, `Expected to find '${expectedCount}' entities of type '${entityType}'`);
+
+		return this;
+	}
+
+	public assertEntityAt(entityType: EntityType, x: number, y: number): SessionAsserter {
+		const entities = this._session.level.entities.getEntitiesAt(x, y);
+
+		const expectedEntities = entities.filter(entity => entity.type === entityType);
+		assert.isTrue(expectedEntities.length > 0, `No entity of type '${entityType}' found at '${x}x${y}'`);
+
+		expectedEntities.forEach(entity => {
+			assert.equal(entity.x, x, `Sanity check: entity of type '${entityType}' placed at ${x}x${y} has different x=${entity.x}`);
+			assert.equal(entity.y, y, `Sanity check: entity of type '${entityType}' placed at ${x}x${y} has different y=${entity.y}`);
+		});
+
+		return this;
+	}
+
+	public assertNoEntityAt(entityType: EntityType, x: number, y: number): SessionAsserter {
+		const entities = this._session.level.entities.getEntitiesAt(x, y);
+
+		const expectedEntities = entities.filter(entity => entity.type === entityType);
+		assert.lengthOf(expectedEntities, 0, `An entity of type '${entityType}' was found at '${x}x${y}'`);
+
+		return this;
+	}
+
+	public assertLevel(callback: { (level: Level): void }): SessionAsserter {
+		callback(this.level);
+
+		return this;
+	}
+}
