e6c63e0eb94bbe9015ea30a4459319818a4163f5
Kieran Millar <kieran@millar.org>
2019-12-27 22:47:01 +0000

    New events folder

--- test/GameLogic/e2e/Events/LevelComplete.test.ts
+++ test/GameLogic/e2e/Events/LevelComplete.test.ts
@@ -1,12 +1,12 @@
 import 'mocha';
-import {FloorType} from '../../../src/GameLogic/Enums/FloorType';
-import {PlayerAction} from '../../../src/GameLogic/Enums/PlayerAction';
-import {TestLevelBuilder} from '../helpers/TestLevelBuilder';
-import {Fireball} from '../../../src/GameLogic/Entities/Fireball';
-import {Direction8} from '../../../src/GameLogic/Enums/Direction8';
-import {TurnEventType} from '../../../src/GameLogic/Enums/TurnEventType';
-import {Iceblock} from '../../../src/GameLogic/Entities/Iceblock';
-import {Protagonist} from '../../../src/GameLogic/Entities/Protagonist';
+import {FloorType} from '../../../../src/GameLogic/Enums/FloorType';
+import {PlayerAction} from '../../../../src/GameLogic/Enums/PlayerAction';
+import {TestLevelBuilder} from '../../helpers/TestLevelBuilder';
+import {Fireball} from '../../../../src/GameLogic/Entities/Fireball';
+import {Direction8} from '../../../../src/GameLogic/Enums/Direction8';
+import {TurnEventType} from '../../../../src/GameLogic/Enums/TurnEventType';
+import {Iceblock} from '../../../../src/GameLogic/Entities/Iceblock';
+import {Protagonist} from '../../../../src/GameLogic/Entities/Protagonist';
 
 describe('GameLogic.e2e - Level completing', () => {
 	it('Level Complete event should be raised when a protagonist steps onto an exit tile', () => {
